b8c470de0b62f8f74ac0586506cd7eda101c6609
Tim Trueblood <timtrueblood@outlook.com>
2024-01-29 18:10:58 -0500

    this model script works

--- scripts/model.py
+++ scripts/model.py
@@ -1,8 +1,17 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
 model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+
+# Example of how to use the model to generate text
+inputs = tokenizer("Hello, how are you?", return_tensors="pt")
+with torch.no_grad():
+    outputs = model.generate(**inputs)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
 
 # Example of how to use the model to generate text
 inputs = tokenizer("Hello, how are you?", return_tensors="pt")
